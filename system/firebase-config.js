99afd72c1e3411554a65c6f52d4c73e15447aa97
Chris Prefontaine <prefontainech@gmail.com>
2026-05-19 15:52:07 -0600

    Enable Fratello Firebase config

--- system/firebase-config.js
+++ system/firebase-config.js
@@ -6,13 +6,13 @@
 // To turn on professional Hub login, create a Firebase web app for Fratello,
 // paste its config values below, and set enabled to true.
 window.FRATELLO_FIREBASE_CONFIG = {
-    enabled: false,
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    enabled: true,
+    apiKey: 'AIzaSyDBZSpwGy2MifMmoKzIz_HYbVEceo2qK7Q',
+    authDomain: 'fratello-hub.firebaseapp.com',
+    projectId: 'fratello-hub',
+    storageBucket: 'fratello-hub.firebasestorage.app',
+    messagingSenderId: '117664640429',
+    appId: '1:117664640429:web:64bcbe431db7995c8d4524',
     measurementId: ''
 };
 
